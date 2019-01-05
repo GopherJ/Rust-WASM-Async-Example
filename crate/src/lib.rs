dff813c24947497f6a2e91351459a6e5c8cddf66
SillyFreak <sillyfreak.ck@gmail.com>
2019-01-06 00:56:45 +0100

    add browser-visible asynchronous output

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -47,16 +47,27 @@ pub async fn run() -> Result<(), JsValue> {
     let window = web_sys::window().expect("should have a Window");
     let document = window.document().expect("should have a Document");
 
+    {
+        let p: web_sys::Node = document.create_element("p")?.into();
+        p.set_text_content(Some("Hello from Rust, WebAssembly, and Webpack!"));
+
+        let body = document.body().expect("should have a body");
+        let body: &web_sys::Node = body.as_ref();
+        body.append_child(&p)?;
+    }
+
     await!(sleep(1000))?;
 
     greet();
 
-    let p: web_sys::Node = document.create_element("p")?.into();
-    p.set_text_content(Some("Hello from Rust, WebAssembly, and Webpack!"));
+    {
+        let p: web_sys::Node = document.create_element("p")?.into();
+        p.set_text_content(Some("... asynchronously!"));
 
-    let body = document.body().expect("should have a body");
-    let body: &web_sys::Node = body.as_ref();
-    body.append_child(&p)?;
+        let body = document.body().expect("should have a body");
+        let body: &web_sys::Node = body.as_ref();
+        body.append_child(&p)?;
+    }
 
     Ok(())
 }
