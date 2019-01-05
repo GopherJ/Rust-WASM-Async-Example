a2e23d90ac980b53dbf0275e49e15142f010a72a
SillyFreak <sillyfreak.ck@gmail.com>
2019-01-05 22:53:49 +0100

    implement sleep in "pure Rust"

--- crate/helper.js
+++ crate/helper.js
@@ -1,6 +1,3 @@
 export const foo = () => {
     console.log("foo");
 };
-
-export const _sleep =
-    timeout => new Promise(resolve => setTimeout(resolve, timeout));
--- crate/src/sleep.rs
+++ crate/src/sleep.rs
@@ -1,19 +1,21 @@
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen(module = "../helper")]
-extern "C" {
-    fn _sleep(millis: u32) -> js_sys::Promise;
-}
-
-pub async fn sleep(millis: u32) -> Result<(), JsValue> {
+pub async fn sleep(millis: i32) -> Result<(), JsValue> {
     use crate::js_compat::promise_to_future;
 
-    await!(promise_to_future(_sleep(millis)))?;
+    let promise = js_sys::Promise::new(&mut move |resolve, _| {
+        let window = web_sys::window().expect("should have a Window");
+        window.set_timeout_with_callback_and_timeout_and_arguments_0(
+            &resolve, millis
+        );
+    });
+
+    await!(promise_to_future(promise))?;
     Ok(())
 }
 
 #[wasm_bindgen(js_name = sleep)]
-pub fn sleep_js(millis: u32) -> js_sys::Promise {
+pub fn sleep_js(millis: i32) -> js_sys::Promise {
     use crate::js_compat::future_to_promise;
 
     future_to_promise(async move {
